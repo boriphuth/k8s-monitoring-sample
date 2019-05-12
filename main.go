9215af50cf930e4206a7f1bb0f3fcfa149dee60f
soichisumi <soichi.sumi@gmail.com>
2019-05-12 12:33:44 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main(){
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("this is simple server"))
+	})
+	http.HandleFunc("/success", func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	http.HandleFunc("/fail", func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Error")
+	})
+	fmt.Println("http server is running on port 8080..")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
